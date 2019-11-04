629c02fdc9d073831cd0821131393330bb436676
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-11-04 16:51:50 +0100

    heroku fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT;
 const app = express();
-
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 
